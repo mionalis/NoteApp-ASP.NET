9d905f6a9a9198681014c17cbaa7b3b229d6ff0a
mionalis <mionallis@gmail.com>
2023-11-01 18:10:02 +0700

    feature(Models): добавлены класс Note и перечисление NoteCategory

---
+++ NoteApp/NoteApp/Models/Note.cs
@@ -0,0 +1,26 @@
+﻿namespace NoteApp.Models
+{
+	public class Note
+	{
+		public Note(string title, string content, NoteCategory category)
+		{
+			Title = title;
+			Content = content;
+			Category = category;
+		}
+
+		public Note()
+		{
+		}
+
+		public string Title { get; set; }
+
+		public string Content { get; set; }
+
+		public NoteCategory Category { get; set; }
+
+		public DateTime CreationTime { get; private set; }
+
+		public DateTime LastModifiedTime { get; private set; }
+	}
+}
---
+++ NoteApp/NoteApp/Models/NoteCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace NoteApp.Models
+{
+	public enum NoteCategory
+	{
+		Work,
+		Home,
+		HealthAndSports,
+		People,
+		Docs,
+		Finance,
+		Other
+	}
+}
