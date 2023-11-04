a54ac47df20608dc1f593422b1094f56a022f837
mionalis <mionallis@gmail.com>
2023-11-04 19:19:24 +0700

    feature(HomeController): реализовано добавление элемента в ListBox

--- NoteApp/NoteApp/Controllers/HomeController.cs
+++ NoteApp/NoteApp/Controllers/HomeController.cs
@@ -8,21 +8,28 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace NoteApp.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public static bool IsEditing { get; set; }
+		public static bool IsEditing { get; set; }
 
-        private List<Note> _noteList { get; set; } = new();
+		// Для тестирования
+		private List<Note>	_noteList = new()
+		{
+			new Note() { Id = 1, Title = "A", IsSelected = false },
+			new Note() { Id = 2, Title = "B", IsSelected = false },
+			new Note() { Id = 3, Title = "C", IsSelected = false },
+		};
 
-        private NoteViewModel _noteViewModel { get; set; } = new();
+		private NoteViewModel _noteViewModel { get; set; } = new();
 
+		[HttpGet]
 		public IActionResult Index()
         {
 	        var citiesSelectListItems = new List<SelectListItem>();
@@ -37,12 +44,15 @@ namespace NoteApp.Controllers
 		        };
 
 		        citiesSelectListItems.Add(selectList);
+		        _noteViewModel.Ids.Add(note.Id);
+		        _noteViewModel.CurrentNote = note;
 	        }
 
 			_noteViewModel.Notes = citiesSelectListItems;
 
 	        return View(_noteViewModel);
-        }
+
+		}
 
 		public IActionResult About()
 		{
@@ -53,7 +63,28 @@ namespace NoteApp.Controllers
         {
 	        IsEditing = true;
 
-	        return View("Index");
+			var newNote = new Note();
+			_noteList.Add(newNote);
+
+			var citiesSelectListItems = new List<SelectListItem>();
+
+			foreach (var note in _noteList)
+			{
+				var selectList = new SelectListItem()
+				{
+					Text = note.Title,
+					Value = note.Id.ToString(),
+					Selected = note.IsSelected
+				};
+
+				citiesSelectListItems.Add(selectList);
+				_noteViewModel.Ids.Add(note.Id);
+				_noteViewModel.CurrentNote = note;
+			}
+
+			_noteViewModel.Notes = citiesSelectListItems;
+
+			return View("Index", _noteViewModel);
         }
 
         public IActionResult EditNote()
