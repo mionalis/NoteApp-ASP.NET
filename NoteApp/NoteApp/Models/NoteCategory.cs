1109d8f38d07f7da45b2acb4854199d020b12686
mionalis <mionallis@gmail.com>
2023-11-12 22:18:53 +0700

    refactoring(NoteCategory): добавлены XML-комментарии

--- NoteApp/NoteApp/Models/NoteCategory.cs
+++ NoteApp/NoteApp/Models/NoteCategory.cs
@@ -5,12 +5,33 @@
 	/// </summary>
 	public enum NoteCategory
 	{
+		/// <summary>
+		/// Работа.
+		/// </summary>
 		Work,
+		/// <summary>
+		/// Дом.
+		/// </summary>
 		Home,
+		/// <summary>
+		/// Здоровье и спорт.
+		/// </summary>
 		HealthAndSports,
+		/// <summary>
+		/// Люди.
+		/// </summary>
 		People,
+		/// <summary>
+		/// Документы.
+		/// </summary>
 		Docs,
+		/// <summary>
+		/// Финансы.
+		/// </summary>
 		Finance,
+		/// <summary>
+		/// Другое.
+		/// </summary>
 		Other
 	}
 }
